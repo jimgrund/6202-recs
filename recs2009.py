cbd03c7ce6693d2b547278d33c72a97cf83eed5c
jimgrund <25105184+jimgrund@users.noreply.github.com>
2018-05-01 22:25:56 -0400

    Update recs2009.py

--- recs2009.py
+++ recs2009.py
@@ -19,6 +19,17 @@ import scipy.stats as stats
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.preprocessing import OneHotEncoder
+from heapq import nlargest
+from sklearn.preprocessing import (MinMaxScaler, StandardScaler)
+from sklearn.linear_model import (LinearRegression, Ridge, 
+                                  Lasso, RandomizedLasso)
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LassoCV, RidgeCV
 
 
 target = ['KWH']
@@ -26,10 +37,10 @@ features = np.array(['EQUIPM','WALLTYPE','ROOFTYPE','YEARMADE','REGIONC','ADQINS
                      'WINDOWS','DOOR1SUM','FUELHEAT','AUDIT','AIA_Zone',
                      'CELLAR','CRAWL','CONCRETE','BASEFIN','BASEHEAT','BASECOOL',
                      'ATTIC','ATTICFIN','ATTCHEAT','ATTCCOOL','AIRCOND','COOLTYPE',
-                     'PRKGPLC1','GARGHEAT','GARGCOOL','HIGHCEIL'
+                     'PRKGPLC1','GARGHEAT','GARGCOOL','HIGHCEIL','SIZRFRI1','AGERFRI1'
                      ])
 
-bin_features = np.array(['TOTSQFT','TOTROOMS','BEDROOMS','MONEYPY','NHSLDMEM','NCOMBATH','NHAFBATH','ACROOMS','HEATROOM','HDD65','CDD65'])
+bin_features = np.array(['TOTSQFT','TOTROOMS','BEDROOMS','MONEYPY','NHSLDMEM','NCOMBATH','NHAFBATH','ACROOMS','HEATROOM','HDD65','CDD65','NUMFRIG'])
 
 raw_recs_df  = pd.read_csv('recs2009_public.csv', low_memory=False)
 
@@ -42,31 +53,74 @@ raw_recs_df = raw_recs_df.replace(-2, 100)
 recs_df = pd.DataFrame(raw_recs_df[np.concatenate((np.append(features,target),bin_features), axis=0)])
 
 
+
+
+##### Plot Y / target
+import seaborn as sns
+sns.set(color_codes=True)
+ax = plt.axes()
+sns.distplot(recs_df[target], rug=True);
+ax.set_title('Distribution of Dependent Variable')
+plt.xlabel('KWH')
+plt.show()
+    
+print("\n\n")
+print("remove outliers to smooth out the distribution")
+recs_df = pd.DataFrame(recs_df[(np.abs(stats.zscore(recs_df[target])) <=3.0)])
+
+ax = plt.axes()
+sns.distplot(recs_df[target], rug=True);
+ax.set_title('Distribution after outlier removal')
+plt.xlabel('KWH')
+plt.show()
+
+
+
+
+#### group the KWH energy usage into bins
+conditions = [
+     (recs_df['KWH'] >= 0)     & (recs_df['KWH'] < 6000),
+     (recs_df['KWH'] >= 6000)  & (recs_df['KWH'] < 9500),
+     (recs_df['KWH'] >= 9500)  & (recs_df['KWH'] < 15000),
+     (recs_df['KWH'] >= 15000)]
+choices=[0,1,2,3]
+
+recs_df['KWH_bin'] = np.select(conditions, choices, default=3)
+
+
+ax = plt.axes()
+sns.distplot(recs_df['KWH_bin'],kde=False);
+ax.set_title('Distribution of KWH_bin')
+plt.xlabel('KWH Bin')
+plt.show()
+
+
+
 # one-hot-encode the categorical attributes
 recs_enc_df = pd.get_dummies(recs_df,columns=features)
 
 # create the features dataframe
-recs_enc_features_df = recs_enc_df.drop('KWH',axis=1,inplace=False)
+recs_enc_features_df = recs_enc_df.drop(['KWH','KWH_bin'],axis=1,inplace=False)
 
 # create a list of the feature names from the column headers
 recs_enc_features = list(recs_enc_features_df.columns.values)
+
 
 # define X to be the features dataframe
 X = recs_enc_features_df
 
 # define Y to be the target
 #Y = recs_enc_df[target]
-#### group the KWH energy usage into bins
-
-conditions = [
-     (recs_enc_df['KWH'] >= 0)     & (recs_enc_df['KWH'] < 6000),
-     (recs_enc_df['KWH'] >= 6000)  & (recs_enc_df['KWH'] < 9500),
-     (recs_enc_df['KWH'] >= 9500)  & (recs_enc_df['KWH'] < 15000),
-     (recs_enc_df['KWH'] >= 15000)]
-choices=[0,1,2,3]
-
-recs_enc_df['KWH_bin'] = np.select(conditions, choices, default=3)
 Y = recs_enc_df[['KWH_bin']]
+
+
+
+
+
+
+
+
+
 
 
 # create the train and test datasets with a split
@@ -74,12 +128,17 @@ X_train, X_test, y_train, y_test = train_test_split( X, Y, test_size=0.3, random
 
 
 
+ind = np.arange(0, 30)
+
+
+###########################################3
+## DecisionTreeRegressor and GINI Index
+
 print("Gini importance")
 ## Feature Importance
 ### Based on max_depth plot, depth = 6 is most ideal
-treereg = DecisionTreeRegressor(max_depth=6, random_state=6103)
+treereg = DecisionTreeRegressor(criterion='friedman_mse', max_depth=6, random_state=6103)
 treereg.fit(X_train, y_train)
-
 
 ### "Gini importance" of each feature: 
 gini_features_df=(pd.DataFrame({'feature':recs_enc_features, 'importance':sorted(treereg.feature_importances_ *1000, reverse = True)}))
@@ -98,13 +157,10 @@ ax.set_title('Gini feature importance')
 plt.show()
     
 
-from heapq import nlargest
-from sklearn.preprocessing import (MinMaxScaler, StandardScaler)
-from sklearn.linear_model import (LinearRegression, Ridge, 
-                                  Lasso, RandomizedLasso)
-from sklearn.neighbors import KNeighborsClassifier
 
 
+
+#### function to plot the feature importance of the various regressors
 def plot_features(feature_list,reg_type):
     df = pd.DataFrame(feature_list)
     fig = plt.figure(figsize=(15,10))
@@ -118,7 +174,9 @@ def plot_features(feature_list,reg_type):
     ax.set_title(reg_type + ' feature importance')
     plt.show()
 
-def rank_to_dict(ranks, names, scaling, order=1):
+
+
+def rank_to_30nlargest(ranks, names, scaling, order=1):
     minmax = MinMaxScaler()
     #std = StandardScaler()
     if (scaling):
@@ -126,7 +184,6 @@ def rank_to_dict(ranks, names, scaling, order=1):
     else:
         ranks = order*np.array(ranks).T
     ranks = map(lambda x: round(x, 2), ranks)
-    #return dict(zip(names, ranks ))
     return nlargest(30,sorted(zip(ranks,names ),reverse=True))
 
 
@@ -182,11 +239,23 @@ def kn_classifier(data,type):
     plt.show()
 
 
-    
+def model_it(data,regressor,model):
+    temp_energy_df = recs_df[get_attribute_listing(data)[1]].copy()
+    temp_energy_enc_df = one_hot_encode(temp_energy_df)
+    # create the train and test datasets with a split
+    tmpX_train, tmpX_test, tmpy_train, tmpy_test = train_test_split( temp_energy_enc_df, Y, test_size=0.3, random_state=6202)
+
+    regressor.fit(tmpX_train, tmpy_train)
+    predict = regressor.predict(tmpX_test)
+    if ( predict.ndim != 2 ):
+        predict = np.array([predict]).T
+    #tmp_mse = np.mean((predict - tmpy_test)**2)
+    tmp_mse = mean_squared_error(tmpy_test, predict)
+    return tmp_mse
+
     
 # RANDOM FOREST Regressor
 print("RandomForestRegressor")
-from sklearn.ensemble import RandomForestRegressor
 
 rf = RandomForestRegressor(n_estimators=10, max_features=4,n_jobs=2)
 rf.fit(X_train, y_train)
@@ -202,11 +271,11 @@ kn_classifier(rand_forest,"Random Forest")
 
 # Recursive Feature Elimination
 #this section takes about half hour to finish
-from sklearn.feature_selection import RFE
-from sklearn.linear_model import LogisticRegression
+
 
 # create a base classifier used to evaluate a subset of attributes
-model = LogisticRegression()
+#model = LogisticRegression()
+model=Ridge(alpha=7)
 # create the RFE model and select 8 attributes
 rfe = RFE(model, 8)
 rfe = rfe.fit(X_train, y_train)
@@ -217,11 +286,16 @@ print("Features sorted by their rank:")
 print(sorted(zip(map(lambda x: round(x, 4), rfe.ranking_), recs_enc_features)))
 
 rfe_data = nlargest(30, sorted(zip(map(lambda x: round(x, 4), rfe.ranking_), recs_enc_features), reverse=True))
-plot_features(rfe_data,"RFE")
-kn_classifier(rfe_data,"RFE")
+plot_features(rfe_data,"RFE Ridge Regression")
+mse = model_it(rfe_data,rfe,"RFE Ridge Regression")
+print("Mean squared error for RFE Ridge Regression: %.2f" % mse)
+#kn_classifier(rfe_data,"RFE")
+#pred=rfe.predict(X_test)
+#mse = np.mean((np.array([pred]).T - y_test)**2)
+#print(mse)
+#print("Mean squared error for RFE: %.2f"
+#      % mean_squared_error(y_test, pred))
 
-
-ind = np.arange(0, 30)
 
 
 
@@ -229,45 +303,98 @@ print("")
 print("Linear Regression")
 lr = LinearRegression(normalize=True)
 lr.fit(X_train, y_train)
-lr_data = rank_to_dict(np.abs(lr.coef_), recs_enc_features, True)
-print(lr_data)
+lr_data = rank_to_30nlargest(np.abs(lr.coef_), recs_enc_features, True)
 
 plot_features(lr_data,"Linear Regression")
-kn_classifier(lr_data,"Linear Regression")
+mse = model_it(lr_data,lr,"Linear Regression")
+print("Mean squared error for Linear Regression: %.2f" % mse)
+#kn_classifier(lr_data,"Linear Regression")
+#pred = lr.predict(X_test)
+#calculating mse
+#mse = np.mean((pred - y_test)**2)
+#print(mse)
+#print("Mean squared error for Linear Regressor: %.2f"
+#      % mean_squared_error(y_test, pred))
+
+
+
+
+## find optimal alpha
+regr_cv = RidgeCV(alphas=[0.1, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0])
+model_cv = regr_cv.fit(X_train, y_train)
+model_cv.alpha_
 
 
 print("") 
 print("Ridge Regression")
 ridge = Ridge(alpha=7)
 ridge.fit(X_train, y_train)
-ridge_data = (rank_to_dict(np.abs(ridge.coef_), recs_enc_features, True))
+ridge_data = (rank_to_30nlargest(np.abs(ridge.coef_), recs_enc_features, True))
 print(ridge_data)
 
 plot_features(ridge_data,"Ridge Regression")
-kn_classifier(ridge_data,"Ridge Regression")
+mse = model_it(ridge_data,ridge,"Ridge Regression")
+print("Mean squared error for Ridge Regression: %.2f" % mse)
+#kn_classifier(ridge_data,"Ridge Regression")
+#pred = ridge.predict(X_test)
+#calculate mse
+#mse = np.mean((pred - y_test)**2)
+#print(mse)
+#print("Mean squared error for Ridge Regressor: %.2f"
+#      % mean_squared_error(y_test, pred))
+
+
+
+## find optimal alpha
+alphas = np.logspace(-3, -1, 30)
+plt.figure(figsize=(5, 3))
+
+for Model in [Lasso, Ridge]:
+    scores = [cross_val_score(Model(alpha), X_train, y_train, cv=3).mean()
+            for alpha in alphas]
+    plt.plot(alphas, scores, label=Model.__name__)
+
+plt.legend(loc='lower left')
+plt.xlabel('alpha')
+plt.ylabel('cross validation score')
+plt.tight_layout()
+plt.show()
+
 
 
 print("")
 print("Lasso")
 lasso = Lasso(alpha=.00075,max_iter=1000)
 lasso.fit(X_train, y_train)
-lasso_data = (rank_to_dict(np.abs(lasso.coef_), recs_enc_features, False))
-print(lasso_data)
+lasso_data = (rank_to_30nlargest(np.abs(lasso.coef_), recs_enc_features, False))
 
 plot_features(lasso_data,"Lasso")
-kn_classifier(lasso_data,"Lasso")
+mse = model_it(lasso_data,lasso,"Lasso")
+print("Mean squared error for Lasso: %.2f" % mse)
+#kn_classifier(lasso_data,"Lasso")
+#pred = lasso.predict(X_test)
+#calculate mse
+#mse = np.mean((np.array([pred]).T - y_test)**2)
+#print(mse)
+#print("Mean squared error for Lasso Regressor: %.2f"
+#      % mean_squared_error(y_test, pred))
+
 
 
 print("")
 print("RandomForestRegressor")
-rf = RandomForestRegressor(n_jobs=2)
+rf = RandomForestRegressor(n_estimators=10, max_features=10, n_jobs=2)
+#rf = RandomForestRegressor(n_jobs=2)
 rf.fit(X,Y)
-random_forest_data = (rank_to_dict(rf.feature_importances_, recs_enc_features, False))
-print(random_forest_data)
+random_forest_data = (rank_to_30nlargest(rf.feature_importances_, recs_enc_features, False))
 
 plot_features(random_forest_data,"Random Forest Regressor")
-kn_classifier(random_forest_data,"Random Forest Regressor")
-
-
-
-
+mse = model_it(random_forest_data,rf,"Random Forest Regressor")
+print("Mean squared error for Random Forest Regressor: %.2f" % mse)
+#kn_classifier(random_forest_data,"Random Forest Regressor")
+#pred = rf.predict(X_test)
+#calculate mse
+#mse = np.mean((np.array([pred]).T - y_test)**2)
+#print(mse)
+#print("Mean squared error for RandomForestRegressor: %.2f"
+#      % mean_squared_error(y_test, pred))
